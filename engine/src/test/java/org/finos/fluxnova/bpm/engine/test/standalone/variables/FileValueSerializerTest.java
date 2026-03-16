caa147cf89830f94dfeb7a22d67938d991cdc0f0
Sowmya, HL <Sowmya.HL@fmr.com>
2026-03-16 16:06:09 +0530

    refactor: update dependencies and fix content type in tests

--- engine/src/test/java/org/finos/fluxnova/bpm/engine/test/standalone/variables/FileValueSerializerTest.java
+++ engine/src/test/java/org/finos/fluxnova/bpm/engine/test/standalone/variables/FileValueSerializerTest.java
@@ -137,7 +137,7 @@ public class FileValueSerializerTest {
 
     assertThat(new String(valueFields.getByteArrayValue(), "UTF-8")).isEqualTo("text");
     assertThat(valueFields.getTextValue()).isEqualTo("simpleFile.txt");
-    assertThat(valueFields.getTextValue2()).isEqualTo("application/octet-stream" + SEPARATOR);
+    assertThat(valueFields.getTextValue2()).isEqualTo("text/plain" + SEPARATOR);
   }
 
   @Test(expected = UnsupportedOperationException.class)
