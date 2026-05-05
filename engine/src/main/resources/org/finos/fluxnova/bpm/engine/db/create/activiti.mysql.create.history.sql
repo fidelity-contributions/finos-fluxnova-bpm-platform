2e1be5c247f3f2195d80745af2a5ed2a3387ded1
Kasina, Yajna <Yajna.Kasina@fmr.com>
2026-05-05 13:30:24 -0600

    feat: add CREATED_BY_ field to attachment-related APIs and database schemas

--- engine/src/main/resources/org/finos/fluxnova/bpm/engine/db/create/activiti.mysql.create.history.sql
+++ engine/src/main/resources/org/finos/fluxnova/bpm/engine/db/create/activiti.mysql.create.history.sql
@@ -205,6 +205,7 @@ create table ACT_HI_ATTACHMENT (
     TENANT_ID_ varchar(64),
     CREATE_TIME_ datetime,
     REMOVAL_TIME_ datetime,
+    CREATED_BY_ varchar(255),
     primary key (ID_)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
