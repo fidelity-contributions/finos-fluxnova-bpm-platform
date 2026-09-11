2816f4428d15ef3e9377399b9aaccdb6d9d12236
Sowmya, HL <Sowmya.HL@fmr.com>
2026-09-11 12:44:20 +0530

    fix: update transient flag parsing to use standard attribute

--- engine/src/main/java/org/finos/fluxnova/bpm/engine/impl/bpmn/parser/BpmnParseUtil.java
+++ engine/src/main/java/org/finos/fluxnova/bpm/engine/impl/bpmn/parser/BpmnParseUtil.java
@@ -134,7 +134,7 @@ public final class BpmnParseUtil {
    */
   public static void parseInputParameterElement(Element inputParameterElement, IoMapping ioMapping) {
     String nameAttribute = inputParameterElement.attribute("name");
-    boolean isTransient = Boolean.parseBoolean(inputParameterElement.attributeNS(BpmnParse.CAMUNDA_BPMN_EXTENSIONS_NS, "isTransient"));
+    boolean isTransient = Boolean.parseBoolean(inputParameterElement.attribute("isTransient"));
     if(nameAttribute == null || nameAttribute.isEmpty()) {
       throw new BpmnParseException("Missing attribute 'name' for inputParameter", inputParameterElement);
     }
@@ -155,7 +155,7 @@ public final class BpmnParseUtil {
    */
   public static void parseOutputParameterElement(Element outputParameterElement, IoMapping ioMapping) {
     String nameAttribute = outputParameterElement.attribute("name");
-    boolean isTransient = Boolean.parseBoolean(outputParameterElement.attributeNS(BpmnParse.CAMUNDA_BPMN_EXTENSIONS_NS, "isTransient"));
+    boolean isTransient = Boolean.parseBoolean(outputParameterElement.attribute("isTransient"));
     if(nameAttribute == null || nameAttribute.isEmpty()) {
       throw new BpmnParseException("Missing attribute 'name' for outputParameter", outputParameterElement);
     }
